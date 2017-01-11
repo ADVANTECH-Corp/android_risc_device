cecb95fa09751b60fed0ea0a9a367ad8edb2e54b
jerryliao <jerry.liao@advantech.com.tw>
2017-01-11 10:13:33 +0800

    Revert "[Display] set default displays as HDMI with VGA"
    This reverts commit 7217e72b370da6728d32302cf191c8955ad08f21.

--- fsl/rom_3420_a1/BoardConfig.mk
+++ fsl/rom_3420_a1/BoardConfig.mk
@@ -108,7 +108,7 @@ $(error "TARGET_USERIMAGES_USE_UBIFS and TARGET_USERIMAGES_USE_EXT4 config open 
 endif
 endif
 
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init fbmem=28M,28M video=mxcfb0:dev=hdmi,1920x1080M@60 video=mxcfb1:dev=lcd,640x480M@55 video=mxcfb2:off vmalloc=400M androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale cma=384M
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0:dev=ldb,bpp=32 video=mxcfb1:off video=mxcfb2:off video=mxcfb3:off vmalloc=256M androidboot.console=ttymxc0 consoleblank=0 androidboot.hardware=freescale cma=384M
 
 ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 #UBI boot command line.
