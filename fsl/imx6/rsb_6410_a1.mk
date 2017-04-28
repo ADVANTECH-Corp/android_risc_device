d636fe7273792c2318af9637e4d40a3e0ac7dc43
zengwei18488 <wei.zeng@advantech.com.cn>
2017-04-28 15:11:53 +0800

    add rsb_6410_a1 mk file

---
+++ fsl/imx6/rsb_6410_a1.mk
@@ -0,0 +1,56 @@
+# This is a FSL Android Reference Design platform based on i.MX6Q ARD board
+# It will inherit from FSL core product which in turn inherit from Google generic
+
+$(call inherit-product, device/fsl/imx6/imx6.mk)
+$(call inherit-product-if-exists,vendor/google/products/gms.mk)
+
+ifneq ($(wildcard device/fsl/rsb_6410_a1/fstab_nand.freescale),)
+$(shell touch device/fsl/rsb_6410_a1/fstab_nand.freescale)
+endif
+
+ifneq ($(wildcard device/fsl/rsb_6410_a1/fstab.freescale),)
+$(shell touch device/fsl/rsb_6410_a1/fstab.freescale)
+endif
+
+# Overrides
+PRODUCT_NAME := rsb_6410_a1
+PRODUCT_DEVICE := rsb_6410_a1
+
+PRODUCT_COPY_FILES += \
+	device/fsl/rsb_6410_a1/init.rc:root/init.freescale.rc \
+        device/fsl/rsb_6410_a1/init.i.MX6Q.rc:root/init.freescale.i.MX6Q.rc \
+	device/fsl/rsb_6410_a1/audio_policy.conf:system/etc/audio_policy.conf \
+	device/fsl/rsb_6410_a1/audio_effects.conf:system/vendor/etc/audio_effects.conf
+
+PRODUCT_COPY_FILES +=	\
+	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6d.bin:system/lib/firmware/vpu/vpu_fw_imx6d.bin 	\
+	external/linux-firmware-imx/firmware/vpu/vpu_fw_imx6q.bin:system/lib/firmware/vpu/vpu_fw_imx6q.bin
+# setup dm-verity configs.
+ifneq ($(BUILD_TARGET_DEVICE),sd)
+ PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/mmcblk1p5
+ $(call inherit-product, build/target/product/verity.mk)
+else 
+ PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/mmcblk1p5
+ $(call inherit-product, build/target/product/verity.mk)
+
+endif
+
+# GPU files
+
+DEVICE_PACKAGE_OVERLAYS := device/fsl/rsb_6410_a1/overlay
+
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_AAPT_CONFIG += xlarge large tvdpi hdpi xhdpi
+
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+	frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+	device/fsl/rsb_6410_a1/required_hardware.xml:system/etc/permissions/required_hardware.xml
+PRODUCT_PACKAGES += AudioRoute
