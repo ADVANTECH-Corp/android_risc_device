0dd0a26e404a4782f4ea07f81436b5e104f4e066
wei.zeng <wei.zeng@advantech.com.cn>
2020-07-14 11:05:04 +0800

    supported EWM163(QCA6174) Wifi

--- fsl/imx8m/rom5720_a1/BoardConfig.mk
+++ fsl/imx8m/rom5720_a1/BoardConfig.mk
@@ -59,46 +59,34 @@ TARGET_BOOTLOADER_POSTFIX := bin
 USE_OPENGL_RENDERER := true
 TARGET_CPU_SMP := true
 
-BOARD_WLAN_DEVICE_UNITE      := UNITE
+BOARD_WLAN_DEVICE            := qcwcn
 WPA_SUPPLICANT_VERSION       := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER  := NL80211
 BOARD_HOSTAPD_DRIVER         := NL80211
 
-# In UNITE mode,Use default macro for bcmdhd and use unite macro for qcom
-ifeq ($(BOARD_WLAN_DEVICE_UNITE), UNITE)
-BOARD_WLAN_DEVICE            := bcmdhd
 BOARD_HOSTAPD_PRIVATE_LIB_QCA           := lib_driver_cmd_qcwcn
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB_QCA    := lib_driver_cmd_qcwcn
-BOARD_HOSTAPD_PRIVATE_LIB_BCM           := lib_driver_cmd_bcmdhd
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB_BCM    := lib_driver_cmd_bcmdhd
-endif
+BOARD_HOSTAPD_PRIVATE_LIB               := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 
-#WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/brcmfmac/parameters/alternative_fw_path"
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
-#WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
 # QCA qcacld wifi driver module
-#BOARD_VENDOR_KERNEL_MODULES += \
-#    $(KERNEL_OUT)/drivers/net/wireless/qcacld-2.0/wlan.ko
-
-# BCM fmac wifi driver module
-#BOARD_VENDOR_KERNEL_MODULES += \
-#    $(KERNEL_OUT)/drivers/net/wireless/broadcom/brcm80211/brcmfmac/brcmfmac.ko \
-#    $(KERNEL_OUT)/drivers/net/wireless/broadcom/brcm80211/brcmutil/brcmutil.ko
-
-#BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(IMX_DEVICE_PATH)/bluetooth
-
-# BCM 1CX BT
-#BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_VENDOR_KERNEL_MODULES += \
+    $(KERNEL_OUT)/drivers/net/wireless/qcacld-2.0/wlan.ko \
+    $(KERNEL_OUT)/drivers/net/wireless/ath/ath10k/ath10k_core.ko \
+    $(KERNEL_OUT)/drivers/net/wireless/ath/ath10k/ath10k_pci.ko
 
 # Qcom 1CQ(QCA6174) BT
-#BOARD_HAVE_BLUETOOTH_QCOM := true
-#BOARD_HAS_QCA_BT_ROME := true
-#BOARD_HAVE_BLUETOOTH_BLUEZ := false
-#QCOM_BT_USE_SIBS := true
-#ifeq ($(QCOM_BT_USE_SIBS), true)
-#    WCNSS_FILTER_USES_SIBS := true
-#endif
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(IMX_DEVICE_PATH)/bluetooth
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BOARD_HAS_QCA_BT_ROME := true
+BOARD_HAVE_BLUETOOTH_BLUEZ := false
+QCOM_BT_USE_SIBS := true
+ifeq ($(QCOM_BT_USE_SIBS), true)
+    WCNSS_FILTER_USES_SIBS := true
+endif
 
 BOARD_USE_SENSOR_FUSION := true
 
@@ -138,10 +126,10 @@ KERNEL_NAME := Image
 BOARD_KERNEL_CMDLINE := init=/init androidboot.gui_resolution=1080p androidboot.console=ttymxc0 androidboot.hardware=freescale androidboot.fbTileSupport=enable cma=$(CMASIZE) androidboot.primary_display=imx-drm firmware_class.path=/vendor/firmware transparent_hugepage=never loop.max_part=7
 
 # Default wificountrycode
-#BOARD_KERNEL_CMDLINE += androidboot.wificountrycode=CN
+BOARD_KERNEL_CMDLINE += androidboot.wificountrycode=CN
 
 # Defaultly evk_8mq use BCM 1CX BCM4356 wifi module, if use QCOM qca9377 module, set androidboot.wifivendor=qca
-BOARD_KERNEL_CMDLINE += androidboot.wifivendor=bcm
+BOARD_KERNEL_CMDLINE += androidboot.wifivendor=qca
 
 ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 ifeq ($(TARGET_USERIMAGES_USE_EXT4),true)
@@ -151,19 +139,19 @@ endif
 
 BOARD_PREBUILT_DTBOIMAGE := out/target/product/rom5720_a1/dtbo-imx8mq.img
 # imx8mq with HDMI display
-TARGET_BOARD_DTS_CONFIG ?= imx8mq:adv-imx8mq-rom5720-a1.dtb
+TARGET_BOARD_DTS_CONFIG ?= imx8mq:adv-imx8mq-rom5720-a1-dual-display.dtb
 # imx8mq with MIPI-HDMI display
-TARGET_BOARD_DTS_CONFIG += imx8mq-mipi:fsl-imx8mq-evk-lcdif-adv7535.dtb
+#TARGET_BOARD_DTS_CONFIG += imx8mq-mipi:fsl-imx8mq-evk-lcdif-adv7535.dtb
 # imx8mq with HDMI and MIPI-HDMI display
-TARGET_BOARD_DTS_CONFIG += imx8mq-dual:fsl-imx8mq-evk-dual-display.dtb
+#TARGET_BOARD_DTS_CONFIG += imx8mq-dual:adv-imx8mq-rom5720-a1-dual-display.dtb
 # imx8mq with MIPI panel display
-TARGET_BOARD_DTS_CONFIG += imx8mq-mipi-panel:fsl-imx8mq-evk-dcss-rm67191.dtb
+#TARGET_BOARD_DTS_CONFIG += imx8mq-mipi-panel:fsl-imx8mq-evk-dcss-rm67191.dtb
 # imx8mq rev. B3 board with HDMI display
-TARGET_BOARD_DTS_CONFIG += imx8mq-b3:fsl-imx8mq-evk-b3.dtb
+#TARGET_BOARD_DTS_CONFIG += imx8mq-b3:fsl-imx8mq-evk-b3.dtb
 # imx8mq rev. B3 board with mipi-hdmi display
-TARGET_BOARD_DTS_CONFIG += imx8mq-mipi-b3:fsl-imx8mq-evk-lcdif-adv7535-b3.dtb
+#TARGET_BOARD_DTS_CONFIG += imx8mq-mipi-b3:fsl-imx8mq-evk-lcdif-adv7535-b3.dtb
 # imx8mq rev. B3 board with MIPI panel display
-TARGET_BOARD_DTS_CONFIG += imx8mq-mipi-panel-b3:fsl-imx8mq-evk-dcss-rm67191-b3.dtb
+#TARGET_BOARD_DTS_CONFIG += imx8mq-mipi-panel-b3:fsl-imx8mq-evk-dcss-rm67191-b3.dtb
 # u-boot target for imx8mq_evk
 TARGET_BOOTLOADER_CONFIG := imx8mq:imx8mq_rom5720a1_2G_android_defconfig
 
